aed9c98ec9f79507c473c5167b14fda6fbfc4c11
zestyroad <zestyroad@naver.com>
2020-10-28 13:37:21 +0900

    Some work

---
+++ starter/StarterCommon/Data/UserInfo.cs
@@ -0,0 +1,10 @@
+﻿using EuNet.Core;
+
+namespace StarterCommon
+{
+    [NetDataObject]
+    public class UserInfo
+    {
+        public string Name;
+    }
+}
---
+++ starter/StarterCommon/RpcInterface/ILoginRpc.cs
@@ -0,0 +1,12 @@
+﻿using EuNet.Rpc;
+using System.Threading.Tasks;
+
+namespace StarterCommon
+{
+    public interface ILoginRpc : IRpc
+    {
+        Task<int> Login(string id);
+        Task<UserInfo> GetUserInfo();
+        // Please add user function here
+    }
+}
