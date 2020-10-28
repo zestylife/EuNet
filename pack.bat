d087e48f4c0958575374cf437bc00e9c04eb345c
zestyroad <zestyroad@naver.com>
2020-10-28 10:34:25 +0900

    Modified nuget package attributes

--- pack.bat
+++ pack.bat
@@ -1,8 +1,2 @@
 dotnet pack -c Release
-copy /y .\src\EuNet\bin\Release\*.nupkg .\nuget
-copy /y .\src\EuNet.Core\bin\Release\*.nupkg .\nuget
-copy /y .\src\EuNet.Rpc\bin\Release\*.nupkg .\nuget
-copy /y .\src\EuNet.Client\bin\Release\*.nupkg .\nuget
-copy /y .\src\EuNet.Server\bin\Release\*.nupkg .\nuget
-copy /y .\src\EuNet.CodeGenerator.Templates\bin\Release\*.nupkg .\nuget
 @PAUSE
