513b4811c304118de04e34f585da8a738bb3319f
zestyroad <zestyroad@naver.com>
2020-12-08 18:15:45 +0900

    Fixed editor script error when installing from PackageManager

--- sandbox/Dev/Client/Assets/Plugins/EuNet/Editor/ExecutionOrderManager.cs
+++ sandbox/Dev/Client/Assets/Plugins/EuNet/Editor/ExecutionOrderManager.cs
@@ -1,24 +1,27 @@
-﻿using System;
+﻿using EuNet.Unity;
+using System;
 using UnityEditor;
-using EuNet.Unity;
 
-[InitializeOnLoad]
-public class ExecutionOrderManager
+namespace EuNet.Editor
 {
-    static ExecutionOrderManager()
+    [InitializeOnLoad]
+    public class ExecutionOrderManager
     {
-        foreach (MonoScript monoScript in MonoImporter.GetAllRuntimeMonoScripts())
+        static ExecutionOrderManager()
         {
-            if (monoScript.GetClass() != null)
+            foreach (MonoScript monoScript in MonoImporter.GetAllRuntimeMonoScripts())
             {
-                foreach (var a in Attribute.GetCustomAttributes(monoScript.GetClass(), typeof(ExecutionOrder)))
+                if (monoScript.GetClass() != null)
                 {
-                    var currentOrder = MonoImporter.GetExecutionOrder(monoScript);
-                    var newOrder = ((ExecutionOrder)a).Order;
-                    if (currentOrder != newOrder)
-                        MonoImporter.SetExecutionOrder(monoScript, newOrder);
+                    foreach (var a in Attribute.GetCustomAttributes(monoScript.GetClass(), typeof(ExecutionOrder)))
+                    {
+                        var currentOrder = MonoImporter.GetExecutionOrder(monoScript);
+                        var newOrder = ((ExecutionOrder)a).Order;
+                        if (currentOrder != newOrder)
+                            MonoImporter.SetExecutionOrder(monoScript, newOrder);
 
-                    //Debug.Log(string.Format("{0} {1}", monoScript.GetClass().Name, newOrder));
+                        //Debug.Log(string.Format("{0} {1}", monoScript.GetClass().Name, newOrder));
+                    }
                 }
             }
         }
--- src/EuNet.Unity/Assets/Plugins/EuNet/Editor/NetViewHandler.cs
+++ src/EuNet.Unity/Assets/Plugins/EuNet/Editor/NetViewHandler.cs
@@ -2,153 +2,157 @@
 using System.Collections.Generic;
 using System.IO;
 using UnityEditor;
+using UnityEditor.Experimental.SceneManagement;
 using UnityEditor.SceneManagement;
 using UnityEngine;
 using Debug = UnityEngine.Debug;
 
-[InitializeOnLoad]
-public class NetViewHandler : EditorWindow
+namespace EuNet.Editor
 {
-    private const string AutoFixViewIdsMenuName = "EuNet/Auto fix view ids on hierarchy changed";
-    private const string AutoFixViewIdsSettingName = "EuNetAutoFixViewIds";
-
-    public static bool IsAutoFixViewIds
+    [InitializeOnLoad]
+    public class NetViewHandler : EditorWindow
     {
-        get { return EditorPrefs.GetBool(AutoFixViewIdsSettingName, true); }
-        set { EditorPrefs.SetBool(AutoFixViewIdsSettingName, value); }
-    }
+        private const string AutoFixViewIdsMenuName = "EuNet/Auto fix view ids on hierarchy changed";
+        private const string AutoFixViewIdsSettingName = "EuNetAutoFixViewIds";
 
-    static NetViewHandler()
-    {
-        EditorApplication.hierarchyChanged -= () => HierarchyChange(true);
-        EditorApplication.hierarchyChanged += () => HierarchyChange(true);
-    }
-
-    [MenuItem(AutoFixViewIdsMenuName)]
-    private static void ToggleAction()
-    {
-        IsAutoFixViewIds = !IsAutoFixViewIds;
-    }
-
-    [MenuItem(AutoFixViewIdsMenuName, true)]
-    private static bool ToggleActionValidate()
-    {
-        Menu.SetChecked(AutoFixViewIdsMenuName, IsAutoFixViewIds);
-        return true;
-    }
-
-    [MenuItem("EuNet/Find and fix viewIds", false, 0)]
-    public static void FindAndFixViewIds()
-    {
-        HierarchyChange(false);
-    }
-
-    internal static void HierarchyChange(bool isAuto)
-    {
-        if (Application.isPlaying)
-            return;
-
-        if (isAuto == true && IsAutoFixViewIds == false)
-            return;
-
-        HashSet<NetView> viewList = new HashSet<NetView>();
-        HashSet<int> usedInstanceViewNumbers = new HashSet<int>();
-        int fixedCount = 0;
-
-        NetView[] findViewList = Resources.FindObjectsOfTypeAll<NetView>();
-
-        foreach (NetView view in findViewList)
+        public static bool IsAutoFixViewIds
         {
-            // 프리팹이나 기타 디스크에 저장되어 있다면
-            if (UnityEditor.Experimental.SceneManagement.PrefabStageUtility.GetPrefabStage(view.gameObject) != null || EditorUtility.IsPersistent(view.gameObject))
-            {
-                if (view.ViewId != 0)
-                {
-                    // 프리팹을 포함한 디스크 저장 오브젝트들은 무조건 viewId를 0으로 사용하자 (생성 시점에서 아이디 발급)
-                    view.ViewId = 0;
-                    view.IsSceneObject = false;
-                    EditorUtility.SetDirty(view);
-                    fixedCount++;
-                }
-            }
-            else
-            {
-                if (view.IsSceneObject == false)
-                {
-                    view.IsSceneObject = true;
-                    EditorUtility.SetDirty(view);
-                    fixedCount++;
-                }
-
-                // 순수한 씬오브젝트라면 리스트에 추가
-                viewList.Add(view);
-            }
+            get { return EditorPrefs.GetBool(AutoFixViewIdsSettingName, true); }
+            set { EditorPrefs.SetBool(AutoFixViewIdsSettingName, value); }
         }
 
-        foreach (NetView view in viewList)
+        static NetViewHandler()
         {
-            if (view.ViewId != 0)
+            EditorApplication.hierarchyChanged -= () => HierarchyChange(true);
+            EditorApplication.hierarchyChanged += () => HierarchyChange(true);
+        }
+
+        [MenuItem(AutoFixViewIdsMenuName)]
+        private static void ToggleAction()
+        {
+            IsAutoFixViewIds = !IsAutoFixViewIds;
+        }
+
+        [MenuItem(AutoFixViewIdsMenuName, true)]
+        private static bool ToggleActionValidate()
+        {
+            Menu.SetChecked(AutoFixViewIdsMenuName, IsAutoFixViewIds);
+            return true;
+        }
+
+        [MenuItem("EuNet/Find and fix viewIds", false, 0)]
+        public static void FindAndFixViewIds()
+        {
+            HierarchyChange(false);
+        }
+
+        internal static void HierarchyChange(bool isAuto)
+        {
+            if (Application.isPlaying)
+                return;
+
+            if (isAuto == true && IsAutoFixViewIds == false)
+                return;
+
+            HashSet<NetView> viewList = new HashSet<NetView>();
+            HashSet<int> usedInstanceViewNumbers = new HashSet<int>();
+            int fixedCount = 0;
+
+            NetView[] findViewList = Resources.FindObjectsOfTypeAll<NetView>();
+
+            foreach (NetView view in findViewList)
             {
-                if (usedInstanceViewNumbers.Contains(view.ViewId))
+                // 프리팹이나 기타 디스크에 저장되어 있다면
+                if (PrefabStageUtility.GetPrefabStage(view.gameObject) != null || EditorUtility.IsPersistent(view.gameObject))
                 {
-                    view.ViewId = 0;
+                    if (view.ViewId != 0)
+                    {
+                        // 프리팹을 포함한 디스크 저장 오브젝트들은 무조건 viewId를 0으로 사용하자 (생성 시점에서 아이디 발급)
+                        view.ViewId = 0;
+                        view.IsSceneObject = false;
+                        EditorUtility.SetDirty(view);
+                        fixedCount++;
+                    }
                 }
                 else
                 {
-                    usedInstanceViewNumbers.Add(view.ViewId);
+                    if (view.IsSceneObject == false)
+                    {
+                        view.IsSceneObject = true;
+                        EditorUtility.SetDirty(view);
+                        fixedCount++;
+                    }
+
+                    // 순수한 씬오브젝트라면 리스트에 추가
+                    viewList.Add(view);
                 }
             }
-        }
 
-        int lastUsedId = 0;
-
-        foreach (NetView view in viewList)
-        {
-            if (view.ViewId == 0)
+            foreach (NetView view in viewList)
             {
-                int nextViewId = NetViewHandler.GetID(lastUsedId, usedInstanceViewNumbers);
+                if (view.ViewId != 0)
+                {
+                    if (usedInstanceViewNumbers.Contains(view.ViewId))
+                    {
+                        view.ViewId = 0;
+                    }
+                    else
+                    {
+                        usedInstanceViewNumbers.Add(view.ViewId);
+                    }
+                }
+            }
 
-                view.ViewId = nextViewId;
+            int lastUsedId = 0;
 
-                lastUsedId = nextViewId;
-                EditorUtility.SetDirty(view);
-                fixedCount++;
+            foreach (NetView view in viewList)
+            {
+                if (view.ViewId == 0)
+                {
+                    int nextViewId = NetViewHandler.GetID(lastUsedId, usedInstanceViewNumbers);
+
+                    view.ViewId = nextViewId;
+
+                    lastUsedId = nextViewId;
+                    EditorUtility.SetDirty(view);
+                    fixedCount++;
+                }
+            }
+
+            if (isAuto == false || fixedCount > 0)
+            {
+                Debug.Log($"ViewIds was fixed [{fixedCount}]");
             }
         }
 
-        if (isAuto == false || fixedCount > 0)
+        public static int GetID(int idOffset, HashSet<int> usedInstanceViewNumbers)
         {
-            Debug.Log($"ViewIds was fixed [{fixedCount}]");
-        }
-    }
-
-    public static int GetID(int idOffset, HashSet<int> usedInstanceViewNumbers)
-    {
-        while (idOffset < NetViews.MaxGenerateViewIdPerSession)
-        {
-            idOffset++;
-            if (!usedInstanceViewNumbers.Contains(idOffset))
+            while (idOffset < NetViews.MaxGenerateViewIdPerSession)
             {
-                break;
+                idOffset++;
+                if (!usedInstanceViewNumbers.Contains(idOffset))
+                {
+                    break;
+                }
             }
+
+            return idOffset;
         }
 
-        return idOffset;
-    }
-
-    public static void LoadAllScenesToFix()
-    {
-        string[] scenes = System.IO.Directory.GetFiles(".", "*.unity", SearchOption.AllDirectories);
-
-        foreach (string scene in scenes)
+        public static void LoadAllScenesToFix()
         {
-            var currentScene = EditorSceneManager.OpenScene(scene);
+            string[] scenes = System.IO.Directory.GetFiles(".", "*.unity", SearchOption.AllDirectories);
 
-            NetViewHandler.HierarchyChange(false);
+            foreach (string scene in scenes)
+            {
+                var currentScene = EditorSceneManager.OpenScene(scene);
 
-            EditorSceneManager.SaveScene(currentScene);
+                NetViewHandler.HierarchyChange(false);
+
+                EditorSceneManager.SaveScene(currentScene);
+            }
+
+            Debug.Log("Corrected scene views where needed.");
         }
-
-        Debug.Log("Corrected scene views where needed.");
     }
 }
